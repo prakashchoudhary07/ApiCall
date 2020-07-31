e9f3b92b880b06e33946672408aad3ee034b3a41
Prakash Choudhary <om.pcprakash@gmail.com>
2020-08-01 04:18:10 +0530

    Env.port bug fixed

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
-const hostname = process.env.PORT || '127.0.0.1';
-const port = 3000
+const express = require('express');
+const app = express();
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('<H1> Please make request to localhost/user/name </H1>'))
+app.get('/', function (req, res) {
+ res.send('<H1> Please make request to localhost/user/name </H1>') 
+})
 
 app.get('/user/:name', function (req, res) {
  res.json(req.params)
